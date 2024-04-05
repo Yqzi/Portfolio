f733696b67ebc8fae1391bb03ad4dd28d54e1e39
Yqzi <y.qazi12@gmail.com>
2024-04-05 00:42:50 -0600

    s

--- Portfolio/src/app/contact-form/contact-form.component.ts
+++ Portfolio/src/app/contact-form/contact-form.component.ts
@@ -1,15 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormBuilder, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
+  imports: [ReactiveFormsModule, CommonModule, MatIconModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
+  contactForm: any;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      number: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
+    });
+  }
+
+  submitForm(): void {
+    if (this.contactForm?.valid) {
+      console.log('Form data:', this.contactForm.value);
+    }
+  }
 
 }
+
